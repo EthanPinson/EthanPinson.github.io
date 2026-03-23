7187cee5668b19b85b8ec8a7240b24793a2f012c
EthanPinson <ejpinson1+s@gmail.com>
2026-03-23 16:59:24 +0000

    pong day 1

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,7 +1,39 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
+
+function pluck_divider() {
+  const overhang = parseFloat($('#divider').css('--overhang'));
+
+  const target = Math.random() * overhang;
+
+  const impact = (overhang - target) / overhang;
+
+  console.log(impact);
+
+  console.log(overhang);
+
+
+  // console.log($('#divider').css('--overhang'))
+  const num = Math.random() * -1 * parseFloat($('#divider').css('--overhang'));
+  // console.log(num);
+  $('#divider').animate({
+      top: -target + 'px',
+      'border-width': 5 * (1 - impact) + 'px'
+    },
+    {
+      step: function( now, fx ) {
+        //console.log('now', now);
+        //console.log('fx', fx);
+      },
+      duration: Math.max((1 - impact) * 1000, 500),
+      complete: function() {
+        $('#divider').animate({'border-width': '5px'}, 250)
+      }
+    })
+}
+
+
 function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
@@ -12,7 +44,9 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+  $('#test').on('click', pluck_divider)
 
+  
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
