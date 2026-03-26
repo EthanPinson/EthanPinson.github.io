6185ea60457ee1b77391eddf664c0d905038e32c
EthanPinson <ejpinson1+s@gmail.com>
2026-03-26 17:58:25 +0000

    waawaw

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -2,14 +2,36 @@
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
 
-function Paddle($board, ball, is_ai) {
+const KEYS = {
+  87: -1,
+  83: 1
+};
+
+function Paddle($board, ball, speed, ball_speed_mod, is_ai) {
   const $$ = $('<div class=paddle>').appendTo($board);
 
+  // position the new paddle to the appropriate place
+  // depending on whether or not this paddle is the ai
   $$.css({
     left: is_ai ? $board.width() - $$.outerWidth() : 0,
     top: $board.height() / 2 - $$.outerHeight() / 2,
     borderColor: $board.css('backgroundColor')
   });
+
+  // function to check if some new y-speed will cause
+  // the paddle to overflow off the board; steps down
+  // the y-speed to a speed that will stop the paddle
+  // from overflowing
+  const cap_y_speed = d_y => {
+    for (let i = 1; i >= 0; i -= 0.25) {
+      const new_y = $$.position().top + d_y * i;
+      if (
+        new_y < 0 ||
+        new_y > $board.height() - $$.outerHeight()
+      ) continue;
+      return d_y * i;
+    }
+  }
 
   function logic(item) {
     const paddle_side_x = $$.position().left
@@ -24,26 +46,34 @@ function Paddle($board, ball, is_ai) {
     const d_y = -ball.$$.position().top - ball.$$.height() / 2
       + $$.position().top + $$.height() / 2;
 
-    // TODO!: SET BOUNDS ON Y COORD
-
-    if (d_x < 0 && Math.abs(d_y) < $$.outerHeight() / 2) ball.speed_x *= -1;
+    if (d_x < 0 && Math.abs(d_y) < $$.outerHeight() / 2)
+      ball.speed_x *= -ball_speed_mod;
 
     if (!is_ai) return;
-    item.speed_y = -Math.sign(d_y) * 0.5;
+    item.speed_y = cap_y_speed(-Math.sign(d_y) * speed);
   }
 
-  return { $$, speed_x: 0, speed_y: 0, logic }
+  function keydown({ which }) {
+    if (is_ai) return; // ai cannot be controlled
+    $$.css('top', `+=${cap_y_speed(KEYS[which] * speed)}`)
+  }
+
+  return { $$, speed_x: 0, speed_y: 0, logic, keydown }
 }
 
-function Ball($board) {
+function Ball($board, speed_mult) {
   const $$ = $('<div id=ball>').appendTo($board);
 
+  // function that returns the ball to the center
+  // of the board
   const center = () => $$.css({
     left: $board.width() / 2 - $$.outerWidth() / 2,
     top: $board.height() / 2 - $$.outerHeight() / 2
   });
 
-  const rng_speed = () => 2 * Math.sign(Math.random() * 2 - 1);
+  // gets a random speed for a speed component
+  // when the ball gets reset to center
+  const rng_speed = () => speed_mult * Math.sign(Math.random() * 2 - 1);
   center();
 
   function logic(item) {
@@ -81,14 +111,21 @@ function runProgram() {
   
   // Game Item Objects
   const $board = $('#board');
-  const ball = Ball($board);
+  const ball = Ball($board, 3);
+
+  $("#test").on('click', () => { ball.$$.css('top', '999px')})
+
+  const items = [
+    Paddle($board, ball, 10, 1.1),
+    Paddle($board, ball, 3, 1.1, true),
+    ball
+  ];
+  items.forEach(item => $('body').on('keydown', (event) => {
+    (item.keydown || (() => {}))(event)
+  }));
 
   // one-time setup
-  const interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL, [
-    Paddle($board, ball),
-    Paddle($board, ball, true),
-    ball
-  ]);
+  const interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL, items);
 
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
 
@@ -102,7 +139,6 @@ function runProgram() {
   */
   function newFrame(items) {
     for (const item of items) {
-      
       item.$$.css({
         left: `+=${item.speed_x}`,
         top: `+=${item.speed_y}`
